c8c58be08287ff3914927ed87ca3898953611772
billo <billonariosh@gmail.com>
2021-08-20 23:44:41 +0900

    2021.08.20 자바 ORM 표준 JPA 프로그래밍 2강 실습

--- README.md
+++
@@ -1,2 +0,0 @@
-# studyjpa
-jpa공부
--- src/main/java/com/billo/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.billo.repository;
-
-import com.billo.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
